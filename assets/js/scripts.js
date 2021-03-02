0f4e35ada6b0ea7a301fec899b233189191dfd6d
Albert Volkman <albert.volkman@gmail.com>
2021-03-02 15:48:26 -0500

    Remove Intercom

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,12 +2,6 @@
     (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-141057075-1", {"cookieDomain":"auto"});ga("set", "anonymizeIp", true);ga("send", "pageview");
 // ]]>
 
-window.intercomSettings = {
-  app_id: "zrqksojs"
-};
-
-(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/zrqksojs';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-
 $('.expanded > span').on('click', function(e) {
   if (e.target != this) return;
   $(this).parent().find('> .menu').toggle();
